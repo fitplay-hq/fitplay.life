e28998964231b973d61cba68b03b1b91b526b084
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-10-03 00:08:06 +0530

    feat:Create Invite by HR

--- app/api/auth/signup/create-invite/route.ts
+++ app/api/auth/signup/create-invite/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { randomUUID } from "crypto";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 
 export async function POST(req: NextRequest) {
@@ -8,12 +10,23 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { companyName, companyAddress, role, employeeCount } = body;
 
+    const session = await getServerSession(authOptions);
+    if (!session || !["ADMIN", "HR"].includes(session.user.role)) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
     if (!["HR", "EMPLOYEE"].includes(role))
       return NextResponse.json({ error: "Invalid role" }, { status: 400 });
 
     let companyId: string;
 
     if (role === "HR") {
+      if (session.user.role !== "ADMIN") {
+        return NextResponse.json(
+          { error: "Only ADMIN can create HR accounts" },
+          { status: 403 }
+        );
+      }
       const company = await prisma.company.create({
         data: { name: companyName, address: companyAddress },
       });
@@ -35,6 +48,12 @@ export async function POST(req: NextRequest) {
     // Create HR or multiple temp EMPLOYEEs
     // ───────────────────────────────
     if (role === "HR") {
+      if (session.user.role !== "ADMIN") {
+        return NextResponse.json(
+          { error: "Only ADMIN can create HR accounts" },
+          { status: 403 }
+        );
+      }
       const tempEmail = `temp_${Date.now()}@example.com`;
       const tempPhone = `${Date.now()}`;
       const user = await prisma.user.create({
@@ -69,6 +88,20 @@ export async function POST(req: NextRequest) {
       const signupLink = `${process.env.VERCEL_URL}/signup?token=${signupToken}`;
       return NextResponse.json({ signupLink });
     } else {
+      if (session.user.role === "HR") {
+        const userId = session.user.id;
+        const data = await prisma.user.findUnique({
+          where: { id: userId },
+          select: { companyId: true },
+        });
+        if (!data?.companyId) {
+          return NextResponse.json(
+            { error: "HR user does not have a companyId" },
+            { status: 400 }
+          );
+        }
+        companyId = data?.companyId;
+      }
       if (!employeeCount || employeeCount < 1)
         return NextResponse.json(
           { error: "employeeCount is required and must be > 0" },

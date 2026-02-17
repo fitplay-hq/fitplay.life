1271b96886df037e481b5c844037215acc0c83c1
jainabhishek2004 <jainabhishekarham@gmail.com>
2026-02-17 16:43:34 +0530

    password reset  company dropdown and other feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -330,6 +330,7 @@ const handleSignIn = () => {
                   </div>
                 </Link>
               )}
+              //changes in main
               
               <Link href="/cart" onClick={() => setIsOpen(false)}>
                 <button className="w-full flex items-center justify-center gap-2 py-2.5 rounded-lg bg-white/5 border border-white/10 hover:bg-white/10 hover:border-white/20 transition-all duration-300">

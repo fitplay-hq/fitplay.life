6e624907b74e0092a31f53cbc15d0724e4bc97ff
dhanushd-27 <dhanushsai2005@gmail.com>
2026-02-25 18:21:16 +0530

    Update Calendly link

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1209,7 +1209,7 @@ export default function SovaHealthPage() {
             <div className="flex-1 w-full bg-white relative overflow-hidden">
               <div
                 className="calendly-inline-widget w-full h-full"
-                data-url="https://calendly.com/expert-consult-fitplay/30min?primary_color=00ff25"
+                data-url="https://calendly.com/expert-consult-fitplay/30min?primary_color=0b8b00"
                 style={{ minWidth: "320px", height: "100%" }}
               ></div>
             </div>
